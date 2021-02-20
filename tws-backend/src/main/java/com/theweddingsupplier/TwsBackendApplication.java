29124dedb762dcb1927514f392f1709e20d75976
HiepHT411 <hoangtuanhiep.04112000@gmail.com>
2021-02-20 09:37:07 +0700

    Create backend project

---
+++ tws-backend/src/main/java/com/theweddingsupplier/TwsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.theweddingsupplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwsBackendApplication.class, args);
+	}
+	
+}
