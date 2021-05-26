c6ec73e0fb667117410acc8153e30d9c72fd0620
HiepHT411 <hoangtuanhiep.04112000@gmail.com>
2021-05-26 15:19:26 +0700

    try to change remote repository

--- tws-backend/src/main/java/com/theweddingsupplier/controller/DashboardController.java
+++ tws-backend/src/main/java/com/theweddingsupplier/controller/DashboardController.java
@@ -3,15 +3,22 @@ package com.theweddingsupplier.controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+//@CrossOrigin(origins ="*", allowedHeaders ="*")
 @RestController
-@CrossOrigin(origins ="*", allowedHeaders ="*")
+@RequestMapping(path = "/api/tws")
 public class DashboardController {
 	
 	@GetMapping("/dashboard")
 	public String testLogin() {
 		System.out.println("Login successfully");
 		return "successCall";
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s", name);
 	}
 }
